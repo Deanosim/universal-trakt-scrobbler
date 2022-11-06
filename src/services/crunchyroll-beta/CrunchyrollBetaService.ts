4084b75491e6f17dbd46ae0d02003fce737f1da8
Deanosim <4834976+Deanosim@users.noreply.github.com>
2022-11-07 01:37:54 +1100

    Update CrunchyrollBetaService.ts
    change beta.crunchyroll.com to crunchyroll.com

--- src/services/crunchyroll-beta/CrunchyrollBetaService.ts
+++ src/services/crunchyroll-beta/CrunchyrollBetaService.ts
@@ -3,8 +3,8 @@ import { Service } from '@models/Service';
 export const CrunchyrollBetaService = new Service({
 	id: 'crunchyroll-beta',
 	name: 'Crunchyroll Beta',
-	homePage: 'https://beta.crunchyroll.com/',
-	hostPatterns: ['*://*.beta.crunchyroll.com/*'],
+	homePage: 'https://crunchyroll.com/',
+	hostPatterns: ['*://*.crunchyroll.com/*'],
 	hasScrobbler: false,
 	hasSync: true,
 	hasAutoSync: true,
